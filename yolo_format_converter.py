6a39405983fead88cdeb16ce7701fa0fb3bcf5e1
Kurtiadam <adamkurti97@gmail.com>
2023-05-15 22:03:51 +0200

    ADD: YOLO format converter script for dataset
    The dataset used for vehicle detection was not in YOLO format. This script writes the labels in YOLO format to train the network.

---
+++ yolo_format_converter.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import imageio.v2 as imageio
+import os
+
+ground_truth = pd.read_csv(r"gt_train.csv",header=None,dtype={0: str})
+
+first_column = ground_truth.iloc[:, 0]
+first_column_int = ground_truth[0].astype(int)
+
+start_path = os.getcwd()
+
+TRAIN_SET_SIZE = 10000
+VAL_SET_RATIO = 0.2
+TEST_SET_RATIO = 0.1
+
+classes = {
+    0:"articulated_truck",
+	1:"bicycle",
+	2:"bus",
+	3:"car",
+	4:"motorcycle",
+	5:"motorized_vehicle",
+	6:"non-motorized_vehicle",
+	7:"pedestrian",
+	8:"pickup_truck",
+	9:"single_unit_truck",
+	10:"work_van"
+}
+key_list = list(classes.keys())
+val_list = list(classes.values())
+
+def write_label(set, index, mode,idx,img):
+    class_name = ground_truth.iloc[idx,1]
+    x_min = ground_truth.iloc[idx,2]
+    y_min = ground_truth.iloc[idx,3]
+    x_max = ground_truth.iloc[idx,4]
+    y_max = ground_truth.iloc[idx,5]
+
+    class_number_pos = val_list.index(class_name)
+    class_number = key_list[class_number_pos]
+
+    # original: xmin, ymin, xmax, ymax -> x_center y_center width height
+    img_height = img.shape[0]
+    img_width = img.shape[1]
+    x_center = round(float((x_min+x_max)/2)/img_width,6)
+    y_center = round(float((y_min+y_max)/2)/img_height,6)
+    bb_height = round(float(y_max - y_min)/img_height,6)
+    bb_width = round(float(x_max - x_min)/img_width,6)
+
+    # class x_center y_center width height
+    row_str = str(class_number) + " " + str(x_center) + " " + str(y_center) + " " + str(bb_width) + " " + str(bb_height)
+    if mode == "new":
+        with open(str(index) + '.txt', 'w') as f:
+            f.write(row_str)
+    if mode == "add":
+        with open(str(index) + '.txt', 'r') as f:
+            txt_data = f.read()
+        txt_data += '\n' + row_str
+        with open(str(index) + '.txt', 'w') as f:
+            f.write(txt_data)
+
+
+def make_set(set, size, start):
+    cnt = 0
+    os.chdir(start_path)
+    if not os.path.exists("labels"):
+        os.mkdir("labels")
+    os.chdir("labels")
+    os.mkdir(set)
+    os.chdir(set)
+    print(start)
+    img_path = r"C:/Users/Adam/Desktop/MIO-TCD-Localization/images_og/train/" + str(first_column[start]) + ".jpg"
+    img = imageio.imread(img_path)
+    prev_index = 100000000
+    
+    for idx, file_name in enumerate(first_column[start:]):
+        if file_name == prev_index:
+            write_label(set,index=ground_truth.iloc[start + idx,0],mode="add",idx=start+idx, img=img)
+        else:
+            img_path = r"C:\Users\Adam\Desktop\MIO-TCD-Localization\images_og\train" + "\\" + str(ground_truth.iloc[start + idx,0]) + ".jpg"
+            print(img_path)
+            img = imageio.imread(img_path)
+            write_label(set,index=ground_truth.iloc[start + idx,0],mode="new",idx=start+idx, img=img)
+        prev_index = file_name
+        if not (img_path == r"C:\Users\Adam\Desktop\MIO-TCD-Localization\images_og\train" + "\\" + str(ground_truth.iloc[start + idx + 1,0]) + ".jpg"):
+            cnt += 1
+            if size == cnt:
+                return idx
+
+
+
+def main():
+    print("Making dataset...")
+    end_idx = make_set(set="train", size = TRAIN_SET_SIZE, start = 0)
+    end_idx_val = make_set(set="val", size = int(TRAIN_SET_SIZE*VAL_SET_RATIO), start = end_idx + 1)
+    end_idx_test = make_set(set="test", size = int(TRAIN_SET_SIZE*TEST_SET_RATIO), start = end_idx+end_idx_val+2)
+    print("Dataset is done!")
+
+if __name__ == "__main__":
+    main()
