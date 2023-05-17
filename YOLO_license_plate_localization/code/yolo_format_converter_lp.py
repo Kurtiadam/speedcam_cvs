a867bcf38042401fa64b2c9578a23149186413cd
Kurtiadam <adamkurti97@gmail.com>
2023-05-17 22:05:26 +0200

    CHG: gitignore for datasets

--- YOLO_license_plate_localization/code/yolo_format_converter_lp.py
+++ YOLO_license_plate_localization/code/yolo_format_converter_lp.py
@@ -2,4 +2,18 @@ import os
 import imageio.v2 as imageio
 import shutil
 
-path = 
+path = '\datasets\license_plate_detection_dataset\images_og'
+
+def write_label():
+    pass
+
+def make_dataset():
+    pass
+
+
+def main():
+    print("Making dataset...")
+    print("Dataset is done!")
+
+if __name__ == "__main__":
+    main()
