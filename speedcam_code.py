900241b59f095b8cd7d72f63993ae948384cb516
Kurtiadam <adamkurti97@gmail.com>
2023-06-04 21:43:18 +0200

    ADD: tesserocr API, burst_photos mode, colored tracking lines, showing cropped vehicles and their license plates.

--- speedcam_code.py
+++ speedcam_code.py
@@ -4,13 +4,17 @@ import numpy as np
 import math
 import time
 from sort import *
-import pytesseract as pt
-from pytesseract import Output
+import os
+import sys
+import tesserocr
+import re
+from PIL import Image
+api = tesserocr.PyTessBaseAPI()
 
 
 class TrafficSpeedCamera:
-    def __init__(self, video_path):
-        self.video_io_handler = VideoIOHandler(video_path)
+    def __init__(self, input_path, input_mode):
+        self.io_handler = IOHandler(input_path)
         self.vehicle_detector = VehicleDetector()
         self.oc_recognizer = OCR()
         self.image_enhancer = LPEnhancer()
@@ -18,22 +22,42 @@ class TrafficSpeedCamera:
         self.object_tracker = ObjectTracker()
         self.speed_estimator = SpeedEstimator()
         self.fps_count = 0
+        self.input_mode = input_mode
+        self.input_path = input_path
         self.last_frame_time = time.time()
 
-    def run(self):
-        while True:
-            ret, frame = self.video_io_handler.cap.read()
-            inph, inpw = frame.shape[:2]
-            frame_ds = cv2.resize(frame, [int(inpw/4), int(inph/4)])
-            frame_ds = frame
-            vehicle_detections = self.vehicle_detector.detect_vehicles(frame_ds)
-            tracked_vehicles = self.object_tracker.track_objects(frame_ds, vehicle_detections)
-            license_plates = self.license_plate_detector.detect_license_plates(frame_ds, tracked_vehicles)
-            # speeds = self.speed_estimator.estimate_speed(frame, tracked_vehicles)
-            self.measure_fps(frame_ds)
-            
-            cv2.imshow("Frame", frame_ds)
-            self.video_io_handler.check_end_stream(ret)
+    def process_frame(self, frame, show_tracking):
+        show_frame = frame.copy()
+        vehicle_detections = self.vehicle_detector.detect_vehicles(show_frame)
+        tracked_vehicles = self.object_tracker.track_objects(show_frame, vehicle_detections, show_tracking)
+        if len(vehicle_detections) != 0:
+            license_plates = self.license_plate_detector.detect_license_plates(frame, show_frame, tracked_vehicles)
+        # speeds = self.speed_estimator.estimate_speed(frame, tracked_vehicles)
+        self.measure_fps(show_frame)
+        cv2.imshow("Frame", show_frame)
+
+    def run(self, show_tracking, ret = True):
+        print("START HERE")
+        if self.input_mode == "burst_photos":
+            file_names = sorted(os.listdir(self.input_path))
+            for file_name in file_names:
+                image_path = os.path.join(self.input_path, file_name)
+                frame = cv2.imread(image_path)
+                self.process_frame(frame, show_tracking)
+                self.io_handler.check_end_stream(ret)
+            ret = False
+            self.io_handler.check_end_stream(ret)
+            print("END HERE")
+
+        elif self.input_mode == "video":
+            while True:
+                ret, frame = self.io_handler.cap.read()
+                self.process_frame(frame)
+                cv2.imshow("Frame", frame)
+                self.io_handler.check_end_stream(ret)
+
+        else:
+            raise NotImplementedError
 
     def measure_fps(self, frame):
         curr_time = time.time()
@@ -51,9 +75,10 @@ class VehicleDetector:
         self.mask = cv2.imread('mask_half.png')
 
     def detect_vehicles(self, frame):
+        print("VEHICLE DETECTION HERE")
         detections = np.empty((0,5))
         # img_region = cv2.bitwise_and(frame, self.mask)
-        vd_results = self.model_vd(frame, stream=True, classes=self.searched_class_indices, conf=0.5, iou=0.9)
+        vd_results = self.model_vd(frame, stream=True, classes=self.searched_class_indices, conf=0.7, iou=0.9, agnostic_nms = True)
         for result in vd_results:
             boxes = result.boxes
             for box in boxes:
@@ -64,18 +89,18 @@ class VehicleDetector:
                 cv2.putText(frame, f'{str(self.model_vd.model.names[cls])} {conf}', [x1_vd, y1_vd], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
                 bbox_arr = np.array([x1_vd, y1_vd, x2_vd, y2_vd, conf])
                 detections = np.vstack((detections, bbox_arr))
-
         return detections
 
 
 class ObjectTracker:
     def __init__(self):
-        self.tracker = Sort(max_age=60, min_hits=10, iou_threshold=0.4)
+        self.tracker = Sort(max_age=60, min_hits=5, iou_threshold=0.4)
         self.vehicle_preds = {}
 
-    def track_objects(self, frame, detections):
+    def track_objects(self, frame, detections, show):
+        print("TRACKING HERE")
         track_results = self.tracker.update(detections)
-        # [x1,y1,x2,y2,idx]
+        # [x1,y1,x2,y2,idx] - X coordinate from left, Y coordinate from top
 
         for vehicle in self.vehicle_preds:
             self.vehicle_preds[vehicle]['tracked'] = False
@@ -98,21 +123,35 @@ class ObjectTracker:
                     self.vehicle_preds[idx]['ocr_conf'] = 0
                     self.vehicle_preds[idx]['tracked'] = False
                     self.vehicle_preds[idx]['tracking_window_opened'] = False
+                    self.vehicle_preds[idx]['speed'] = 0
         
             
             # if conf > self.vehicle_preds[idx]['vd_conf']:
                 # self.vehicle_preds[idx]['vd_conf'] = conf
             self.vehicle_preds[idx]['vd_bbox_coords'] = bboxes_int
-            self.vehicle_preds[idx]['vd_center'] = center
             self.vehicle_preds[idx]['tracked'] = True
+            # Draw travelled track lines
+            if show:
+                self.vehicle_preds[idx]['vd_center'].append(center)
+                if idx % 2 == 0:
+                    R = 255
+                    B = 0
+                    G = 0
+                else:
+                    R = 0
+                    B = 0
+                    G = 255
+                for i in range(len(self.vehicle_preds[idx]['vd_center']) - 1):
+                    center_x = self.vehicle_preds[idx]['vd_center'][i]
+                    center_x = tuple(map(int, center_x))
+                    center_y = self.vehicle_preds[idx]['vd_center'][i + 1]
+                    center_y = tuple(map(int, center_y))
+                    cv2.line(frame, center_y, center_x, (B, G, R), thickness=2)
+                cv2.putText(frame, f'{str(idx)}', [bboxes_int[2], bboxes_int[1]], cv2.FONT_HERSHEY_SIMPLEX, 1, (B, G, R), 2)
 
-            # # Draw travelled track lines
-            # for i in range(len(self.vehicle_preds[idx]['vd_center']) - 1):
-            #     center_x = self.vehicle_preds[idx]['vd_center'][i]
-            #     center_x = tuple(map(int, center_x))
-            #     center_y = self.vehicle_preds[idx]['vd_center'][i + 1]
-            #     center_y = tuple(map(int, center_y))
-            #     cv2.line(frame, center_y, center_x, (255, 0, 0), thickness=2)
+            else:
+                self.vehicle_preds[idx]['vd_center'] = center
+                cv2.putText(frame, f'{str(idx)}', [bboxes_int[2], bboxes_int[1]], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
 
         return self.vehicle_preds
 
@@ -124,55 +163,63 @@ class LicensePlateDetector:
         self.oc_recognizer = oc_recognizer
         self.image_enhancer = image_enhancer
 
-    def detect_license_plates(self, frame, vehicle_preds):
-        frame_h = int(frame.shape[0]/2)
+    def detect_license_plates(self, frame, show_frame, vehicle_preds):
         for idx in vehicle_preds.keys():
-            bbox = vehicle_preds[idx]['vd_bbox_coords']
+            bbox_vd = vehicle_preds[idx]['vd_bbox_coords']
             # [x1,y1,x2,y2,idx]
-            print("ASDASDA", vehicle_preds[idx]['vd_center'][1], frame_h)
-            if vehicle_preds[idx]['vd_center'][1] < frame_h:
-                cropped_vehicle = np.array(frame[bbox[1]:bbox[3], bbox[0]:bbox[2]])
-
-                # Visualizing vehicles
-                if vehicle_preds[idx]['tracked'] == True:
-                    cv2.imshow(str(idx), cropped_vehicle)
-                    vehicle_preds[idx]['tracking_window_opened'] = True
-                elif vehicle_preds[idx]['tracked'] == False and vehicle_preds[idx]['tracking_window_opened'] == True:
-                    cv2.destroyWindow(str(idx))
-                    vehicle_preds[idx]['tracking_window_opened'] = False
-
-                lp_preds = self.model_lp(cropped_vehicle, imgsz=320)
+            print("LICENSE PLATE DETECTION HERE")
+            cropped_vehicle = np.array(frame[bbox_vd[1]:bbox_vd[3], bbox_vd[0]:bbox_vd[2]])
+            # Visualizing vehicles
+            if vehicle_preds[idx]['tracked'] == True:
+                cv2.imshow(str(idx), cropped_vehicle)
+                x_w, y_w, _, h_w = cv2.getWindowImageRect(str(idx))
+                lp_text = vehicle_preds[idx]['lp_text']
+                cv2.putText(show_frame, lp_text, [bbox_vd[0],bbox_vd[3]+25], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 255), 2)
+                vehicle_preds[idx]['tracking_window_opened'] = True
+            elif vehicle_preds[idx]['tracked'] == False and vehicle_preds[idx]['tracking_window_opened'] == True:
+                cv2.destroyWindow(str(idx))
+                if cv2.getWindowProperty(str(idx) + " license plate", cv2.WND_PROP_VISIBLE) >=1:
+                    cv2.destroyWindow(str(idx) + " license plate")
+                vehicle_preds[idx]['tracking_window_opened'] = False
+            if vehicle_preds[idx]['tracked']:
+                lp_preds = self.model_lp(cropped_vehicle, imgsz=320, iou=0.5)
                 for result in lp_preds:
                             boxes = result.boxes
                             for box in boxes:
                                 if len(box.xyxy[0]) != 0:
                                     x1_lp, y1_lp, x2_lp, y2_lp = tuple(map(int, box.xyxy[0]))
                                     bbox_arr_lp = np.array([x1_lp, y1_lp, x2_lp, y2_lp])
+                                    lp_center = (int(bbox_vd[0] + (x1_lp+x2_lp)/2),int(bbox_vd[1] + (y1_lp+y2_lp)/2))
                                     vehicle_preds[idx]['lp_bbox_coords'] = bbox_arr_lp
                                     cropped_lp = np.array(cropped_vehicle[y1_lp:y2_lp, x1_lp:x2_lp])
-
                                     conf = math.ceil((box.conf[0]*100))/100
                                     vehicle_preds[idx]['lp_conf'] = conf
 
                                     # Visualizing license plates
                                     if vehicle_preds[idx]['tracked'] == True:
                                         cv2.imshow(str(idx) + " license plate", cropped_lp)
+                                        cv2.moveWindow(str(idx) + " license plate", x_w, y_w + h_w)
+
                                         vehicle_preds[idx]['tracking_window_opened'] = True
                                     elif vehicle_preds[idx]['tracked'] == False and vehicle_preds[idx]['tracking_window_opened'] == True:
                                         cv2.destroyWindow(str(idx) + " license plate")
                                         vehicle_preds[idx]['tracking_window_opened'] = False
                                     
-                                    enhanced_frame = self.image_enhancer.enhance_image(cropped_lp)
-                                    cv2.imshow('Enchanced final', enhanced_frame)
-                                    lp_text, ocr_conf = self.oc_recognizer.read_license_plate(enhanced_frame)
+                                    if lp_center[0] >= (frame.shape[1]*0.05):
+                                        enhanced_frame = self.image_enhancer.enhance_image(cropped_lp)
+                                        lp_text, ocr_conf = self.oc_recognizer.read_license_plate(enhanced_frame)
+                                        if vehicle_preds[idx]['lp_text'] == "" and 7 <= len(lp_text) <= 8:
+                                            vehicle_preds[idx]['lp_text'] = lp_text
+                                            vehicle_preds[idx]['ocr_conf'] = ocr_conf
+                                        elif ocr_conf > vehicle_preds[idx]['ocr_conf'] and 7 <= len(lp_text) <= 8:
+                                            vehicle_preds[idx]['lp_text'] = lp_text
+                                            vehicle_preds[idx]['ocr_conf'] = ocr_conf
+                                    else:
+                                        print(idx, "OCR skipped - LP out of frame")
 
-                                    if ocr_conf > vehicle_preds[idx]['ocr_conf']:
-                                        vehicle_preds[idx]['lp_text'] = lp_text
-                                        vehicle_preds[idx]['ocr_conf'] = ocr_conf
-                print("\nPRED:", vehicle_preds)                        
-            else:
-                print("\nPRED:", vehicle_preds)
-                continue
+        for key, value in vehicle_preds.items():
+            print(key, value)
+        print("\n")
 
         return vehicle_preds
 
@@ -182,16 +229,36 @@ class LicensePlateDetector:
 
 class LPEnhancer:
     def __init__(self) -> None:
-        self.rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (13, 5))
+        self.rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (15, 5))
 
     def enhance_image(self, frame):
-        w,h = frame.shape[:2]
-        print("ASDASDA", w,h)
+        h,w = frame.shape[:2]
         test_lp_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        bil = cv2.bilateralFilter(test_lp_gray, 11, 17, 17)
+        # cv2.imshow("2 - Bilateral Filter", bil)
         blackhat = cv2.morphologyEx(test_lp_gray, cv2.MORPH_BLACKHAT, self.rectKern)
+        # cv2.imshow("Blackhat", blackhat)
+        # equalized_image = cv2.equalizeHist(blackhat)
+        # cv2.imshow("eq", equalized_image)
         # Angle correction
-        edges = cv2.Canny(blackhat, 50, 150, apertureSize=3)
-        lines = cv2.HoughLines(blackhat, 1, np.pi / 180, 50)
+        edges = cv2.Canny(bil, 10, 50, apertureSize=3)
+        lines = cv2.HoughLines(edges, 1, np.pi / 180, 50)
+
+        # if lines is not None:
+        #     for line in lines:
+        #         rho, theta = line[0]
+        #         a = np.cos(theta)
+        #         b = np.sin(theta)
+        #         x0 = a * rho
+        #         y0 = b * rho
+        #         x1 = int(x0 + 1000 * (-b))
+        #         y1 = int(y0 + 1000 * (a))
+        #         x2 = int(x0 - 1000 * (-b))
+        #         y2 = int(y0 - 1000 * (a))
+                
+        #         cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+        # cv2.imshow("With lines", frame)
+
         angles = []
         try:
             for line in lines:
@@ -201,12 +268,14 @@ class LPEnhancer:
             predominant_angle = np.median(angles)
         except:
             predominant_angle = 90
-        rotation_matrix = cv2.getRotationMatrix2D((w / 2, h / 2),predominant_angle-90, 1)
-        angle_corrected = cv2.warpAffine(blackhat, rotation_matrix, (h, w))
+        rotation_matrix = cv2.getRotationMatrix2D((h / 2, w / 2),predominant_angle-90, 1)
+        angle_corrected = cv2.warpAffine(blackhat, rotation_matrix, (w, h))
 
-        # test_lp_gray_bw = cv2.adaptiveThreshold(angle_corrected,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,1,5)
-        enhanced_frame = frame
-        # enhanced_frame = cv2.erode(test_lp_gray_bw, (2,2), iterations=1)
+        # test_lp_gray_bw = cv2.threshold(angle_corrected, 0, 255,cv2. THRESH_BINARY | cv2.THRESH_OTSU)[1]
+        # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 2))
+        # enhanced_frame = cv2.erode(test_lp_gray_bw, kernel, iterations=1)
+        enhanced_frame = angle_corrected
+        # cv2.imshow("Enhanched", enhanced_frame)
 
         return enhanced_frame
 
@@ -214,52 +283,58 @@ class LPEnhancer:
 class OCR():
     def __init__(self) -> None:
         self.alphanumeric = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-"
-        self.options = "-c tessedit_char_whitelist={}".format(self.alphanumeric)
-        self.options += " --psm {}".format(7)
+        # self.options = "-c tessedit_char_whitelist={}".format(self.alphanumeric)
+        # self.options += " --psm {}".format(7)
     
     def read_license_plate(self, frame):
-        data = pt.image_to_data(frame, config=self.options, output_type=Output.DICT)
-        confidences = data['conf']
-        confidences = [float(c) for c in confidences if c != '-1']
-        conf = np.mean(confidences) / 100
-        text = data['text']
-        text = ' '.join(text).strip()
+        # data = pt.image_to_data(frame, config=self.options, output_type=Output.DICT)
+        # confidences = data['conf']
+        # confidences = [float(c) for c in confidences if c != '-1']
+        # conf = np.mean(confidences) / 100
+        # text = data['text']
+        # text = ' '.join(text).strip()
+
+        with tesserocr.PyTessBaseAPI(psm=tesserocr.PSM.SINGLE_LINE) as api:
+            api.SetVariable('tessedit_char_whitelist', self.alphanumeric)
+            image = Image.fromarray(frame)
+            api.SetImage(image)
+            text = api.GetUTF8Text()
+            text = text.strip()
+            confidences = api.AllWordConfidences()
+            confidences = [float(c) for c in confidences]
+            conf = np.mean(confidences) / 100
+            print("TESSEROCR PRED: ", text, "CONF: ", conf)
         return text, conf
 
 
 class SpeedEstimator:
     def __init__(self):
         # (1146x894) bottom left lane piece - (1470,358) upper right lane piece
-        ref_points = np.array([[1470,358],[1146,894]])
-        dist_in_meters = 1+1.75+1+1.75+1
-        dist_in_pixels = math.sqrt(ref_points[0][0]-ref_points[1][0])**2+(ref_points[0][1]-ref_points[1][1])**2
-        pixel_meter_ratio = dist_in_pixels/dist_in_meters
+        self.ref_points = np.array([[1470,358],[1146,894]])
+        self.dist_in_meters = 1+1.75+1+1.75+1
+        self.dist_in_pixels = math.sqrt(self.ref_points[0][0]-self.ref_points[1][0])**2+(self.ref_points[0][1]-self.ref_points[1][1])**2
+        self.pixel_meter_ratio = self.dist_in_pixels/self.dist_in_meters
+        self.time_per_frame = 0.1
 
     def estimate_speed(self, frame, tracked_objects):
         pass
 
-@staticmethod
-class FPSMeasurment:
-        def measure_fps(begin_time, frame):
-            end_time = time.time()
-            frame_time = end_time-begin_time
-            fps = int(1/frame_time)
-            cv2.putText(frame, str(fps), [frame.shape[0]-100, frame.shape[0]-50], cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+class IOHandler:
+    def __init__(self, input_path):
+        self.input_path = input_path
+        self.cap = cv2.VideoCapture(input_path)
 
-
-class VideoIOHandler:
-    def __init__(self, video_path):
-        self.video_path = video_path
-        self.cap = cv2.VideoCapture(video_path)
 
     def check_end_stream(self, ret):
-        if cv2.waitKey(100) & 0xFF == ord('q') or not ret:
+        if cv2.waitKey(1) & 0xFF == ord('q') or not ret:
             self.cap.release()
             cv2.destroyAllWindows()
+            sys.exit()
+
 
 def main():
-    speed_camera = TrafficSpeedCamera('./samples/Videok/second_try.avi')
-    speed_camera.run()
+    speed_camera = TrafficSpeedCamera(r"C:\Users\Adam\Desktop\speedcam_samples\06.03\FAST_2", "burst_photos")
+    speed_camera.run(show_tracking = True)
 
 
 if __name__ == '__main__':
